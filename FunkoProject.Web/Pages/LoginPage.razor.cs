0eac7458405919feb59e5d8a65f7b42fd16c8421
sdubowski <szymon.dubowski@gmail.com>
2024-09-14 01:28:05 +0200

    account service

---
+++ FunkoProject.Web/Pages/LoginPage.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+
+namespace FunkoProject.Web.Pages;
+
+public class LoginPageBase : ComponentBase
+{
+    protected LoginModel LoginModel { get; set; } = new LoginModel();
+
+    protected void HandleValidSubmit()
+    {
+        // Tutaj dodaj logikę logowania
+        Console.WriteLine($"Próba logowania: {LoginModel.Username}");
+    }
+}
+
+public class LoginModel
+{
+    [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Hasło jest wymagane")]
+    public string Password { get; set; }
+}
---
+++ FunkoProject.Web/Services/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using FunkoProject.Web.Models;
+
+namespace FunkoProject.Web.Services;
+
+// AccountService.cs
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+public class AccountService
+{
+    private readonly HttpClient _httpClient;
+
+    public AccountService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string> Login(User user)
+    {
+        var response = await _httpClient.PostAsJsonAsync("api/Account/login", user);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
